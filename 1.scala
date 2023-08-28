c9d9b880436d07a1ad42b24fe3f89fb80960a467
Akila Kushantha <akila.kushantha@gmail.com>
2023-08-28 19:28:52 +0530

    Add assignment 9

---
+++ 1.scala
@@ -0,0 +1,13 @@
+class Rational(var n:Int, var d:Int){
+    var numerator = n
+    var denominator = d
+
+    def neg():Unit = numerator = numerator * -1
+}
+
+def main(args: Array[String]):Unit={
+    var r = new Rational(3, 7)
+    r.neg()
+    println("Numerator: "+r.numerator+"   Denominator: "+r.denominator)
+    println(r.numerator+"/"+r.denominator)
+}
